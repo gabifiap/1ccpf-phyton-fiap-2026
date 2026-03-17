249fe0ed4d0e8c381edda1de803a725609f25ab0
gabifiap <batistagabi351@gmail.com>
2026-03-17 11:42:08 -0300

    aula 02

---
+++ aula 02-variáveis e operadores/Aula 02 variáveis.py
@@ -0,0 +1,30 @@
+print ("Olá Mundo")
+
+print(7+4)
+print("7+4")
+print("7"+"4") #CONCATENAÇÃO DE STRINGS
+
+
+# VARIÁVEIS
+nome= "Alexandre"
+idade= "25"
+peso= 70.2
+
+print ("Oiii",nome, idade, peso)
+print ("Oiii, {nome}")
+
+# INPUT -- SIMULA FORMS NO CMD
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: ")) #Com o int retorna numeros
+peso = input("Digite seu peso: ")
+
+print(nome,  idade,  peso)
+
+#Atividade 1
+nome = input("Digite seu nome: ")
+print (f"Boas vindas {nome}!!")
+
+#Atividade 2
+Data de nascimento = input("Digite sua data de nascimento: ")
+print (f"Qual sua data de nascimento?: {Data de nascimento}")
+
